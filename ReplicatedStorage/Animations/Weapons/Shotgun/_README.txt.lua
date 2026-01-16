506e5158db3309f719dc25503913e81bc1fec55f
Carnajaytwa <82830372+Carnage-Joker@users.noreply.github.com>
2026-01-17 09:27:32 +1100

    Updated ReplicatedStorage/Animations/Weapons/Shotgun/_README.txt.lua

---
+++ ReplicatedStorage/Animations/Weapons/Shotgun/_README.txt.lua
@@ -0,0 +1,5 @@
+-- @ScriptType: Script
+Animation placeholders for Shotgun
+
+These .txt files represent Animation instances that should be created in Roblox Studio.
+Each animation should have an AnimationId set to the corresponding uploaded animation asset.
